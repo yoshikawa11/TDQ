8cf3695f5b044718cf45914e5e1f6bb836b107e2
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-05-28 12:35:59 +0900

    tdq-6:mission3

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 function main() {
-  const { title, hidden } = { title: "タイトル", hidden: true };
-  console.log(title, hidden);
+  const tdqResult = { data: [1, 2], error: null };
+  const tdqData = tdqResult.data;
+  const tdqError = tdqResult.error;
+  console.log(tdqData, tdqError);
+
+  const { data: newData, error: newError } = { data: [1, 2], error: null };
+  console.log(newData, newError);
 }
 
 main();
