1eadb83563dc83f683192a5d0b7edf53599a42bf
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-05-28 12:20:11 +0900

    tdq-5:finished

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [📗TDQ-002 JavaScriptの開発環境構築](https://minerva.mamansoft.net/%F0%9F%93%97TDQ/%F0%9F%93%97TDQ-002+JavaScript%E3%81%AE%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E6%A7%8B%E7%AF%89)
 - [📗TDQ-003 変数と宣言](https://minerva.mamansoft.net/%F0%9F%93%97TDQ/%F0%9F%93%97TDQ-003+%E5%A4%89%E6%95%B0%E3%81%A8%E5%AE%A3%E8%A8%80)
 - [📗TDQ-004 データ型](https://minerva.mamansoft.net/%F0%9F%93%97TDQ/%F0%9F%93%97TDQ-004+%E3%83%87%E3%83%BC%E3%82%BF%E5%9E%8B)
+- [📗TDQ-005 等価演算子と厳密等価演算子](https://minerva.mamansoft.net/%F0%9F%93%97TDQ/%F0%9F%93%97TDQ-005+%E7%AD%89%E4%BE%A1%E6%BC%94%E7%AE%97%E5%AD%90%E3%81%A8%E5%8E%B3%E5%AF%86%E7%AD%89%E4%BE%A1%E6%BC%94%E7%AE%97%E5%AD%90)
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+function isDeepEquals(objA, objB) {
+  const sortA = Object.keys(objA).sort();
+  const sortB = Object.keys(objB).sort();
+
+  if (sortA.length !== sortB.length) {
+    return false;
+  }
+  for (let i = 0; i < sortA.length; i++) {
+    const key = sortA[i];
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test cases for the isDeepEquals function
 function main() {
-  const s = null;
-  console.log(typeof s);
+  console.log(isDeepEquals({}, {})); // true
+  console.log(isDeepEquals({ id: 1, name: "a" }, { id: 1, name: "a" })); // true
+  console.log(isDeepEquals({ id: 1, name: "a" }, { name: "a", id: 1 })); // true
+  console.log(isDeepEquals({ id: 1, name: "a" }, { id: 2, name: "a" })); // false
 }
 
 main();
