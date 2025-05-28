3825c3bef411e34eaa1d881e0f293ec25f0ae49f
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-05-28 13:14:45 +0900

    tdq-7:mission1

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function hoge(args) {
-  const { id, name = "No name" } = args;
-
-  console.log(id, name);
+/**
+ * 引数がすべて空文字ではないことを確認します
+ * @param {string} str1
+ * @param {string} str2
+ * @param {string} str3
+ * @returns {boolean} 結果
+ */
+function notIncludesEmpty(str1, str2, str3) {
+  return str1 != "" && str2 != "" && str3 != "";
 }
 
-hoge({ id: 1, name: "test" }); // Outputs: 1 test
-hoge({ id: 2 }); // Outputs: 2 No name
-hoge({ id: 3, name: "example", student: true }); // Outputs: 3 example
+function main() {
+  console.log(notIncludesEmpty("a", "b", "c")); // true
+  console.log(notIncludesEmpty("a", "", "c")); // false
+  console.log(notIncludesEmpty("", "", "")); // false
+  console.log(notIncludesEmpty("a", "b", null)); // true
+}
+
+main();
